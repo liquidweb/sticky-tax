83ed2c6e2a8a969bc3fbe74970f1739da571edb4
Steve Grunwell <sgrunwell@liquidweb.com>
2017-08-28 18:48:10 +0000

    Abstract the Ajax request setup to a protected make_ajax_request() method, enabling each test case to only override the properties needed

--- tests/test-ajax.php
+++ tests/test-ajax.php
@@ -13,116 +13,73 @@ use LiquidWeb\StickyTax\Ajax as Ajax;
 class AjaxTest extends WP_Ajax_UnitTestCase {
 
 	public function test_ajax_success() {
-		$_POST['nonce']     = wp_create_nonce( 'sticky-tax' );
-		$_POST['action']    = 'stickytax_get_id_from_name';
-		$_POST['term_name'] = 'Uncategorized';
-		$_POST['term_type'] = 'category';
+		$response = $this->make_ajax_request();
 
-		try {
-			$this->_handleAjax( 'stickytax_get_id_from_name' );
-		} catch ( WPAjaxDieContinueException $e ) {
-			return;
-		}
-
-		// Check that the exception was thrown.
-		$response = json_decode( $this->_last_response );
-
-		$this->assertInternalType( 'object', $response );
-		$this->assertObjectHasAttribute( 'success', $response );
 		$this->assertTrue( $response->success );
 		$this->assertObjectHasAttribute( 'data', $response );
 	}
 
 	public function test_ajax_error_bad_nonce() {
-		$_POST['nonce']     = uniqid();
-		$_POST['action']    = 'stickytax_get_id_from_name';
-		$_POST['term_name'] = 'Uncategorized';
-		$_POST['term_type'] = 'category';
+		$response = $this->make_ajax_request( [
+			'nonce' => uniqid(),
+		] );
 
-		try {
-			$this->_handleAjax( 'stickytax_get_id_from_name' );
-		} catch ( WPAjaxDieContinueException $e ) {
-			return;
-		}
-
-		// Check that the exception was thrown.
-		$response = json_decode( $this->_last_response );
-
-		$this->assertInternalType( 'object', $response );
-		$this->assertObjectHasAttribute( 'success', $response );
 		$this->assertFalse( $response->success );
-		$this->assertObjectHasAttribute( 'data', $response );
-		$this->assertObjectHasAttribute( 'errcode', $response->data );
 		$this->assertEquals( $response->data->errcode, 'BAD_NONCE' );
 	}
 
 	public function test_ajax_error_no_term_name() {
-		$_POST['nonce']     = wp_create_nonce( 'sticky-tax' );
-		$_POST['action']    = 'stickytax_get_id_from_name';
-		$_POST['term_name'] = '';
-		$_POST['term_type'] = 'category';
+		$response = $this->make_ajax_request( [
+			'term_name' => '',
+		] );
 
-		try {
-			$this->_handleAjax( 'stickytax_get_id_from_name' );
-		} catch ( WPAjaxDieContinueException $e ) {
-			return;
-		}
-
-		// Check that the exception was thrown.
-		$response = json_decode( $this->_last_response );
-
-		$this->assertInternalType( 'object', $response );
-		$this->assertObjectHasAttribute( 'success', $response );
 		$this->assertFalse( $response->success );
-		$this->assertObjectHasAttribute( 'data', $response );
-		$this->assertObjectHasAttribute( 'errcode', $response->data );
 		$this->assertEquals( $response->data->errcode, 'NO_TERM_NAME' );
 	}
 
 	public function test_ajax_error_no_term_type() {
-		$_POST['nonce']     = wp_create_nonce( 'sticky-tax' );
-		$_POST['action']    = 'stickytax_get_id_from_name';
-		$_POST['term_name'] = 'Uncategorized';
-		$_POST['term_type'] = '';
+		$response = $this->make_ajax_request( [
+			'term_type' => '',
+		] );
 
-		try {
-			$this->_handleAjax( 'stickytax_get_id_from_name' );
-		} catch ( WPAjaxDieContinueException $e ) {
-			return;
-		}
-
-		// Check that the exception was thrown.
-		$response = json_decode( $this->_last_response );
-
-		$this->assertInternalType( 'object', $response );
-		$this->assertObjectHasAttribute( 'success', $response );
 		$this->assertFalse( $response->success );
-		$this->assertObjectHasAttribute( 'data', $response );
-		$this->assertObjectHasAttribute( 'errcode', $response->data );
 		$this->assertEquals( $response->data->errcode, 'NO_TERM_TYPE' );
 	}
 
 	public function test_ajax_error_no_term_found() {
-		$_POST['nonce']     = wp_create_nonce( 'sticky-tax' );
-		$_POST['action']    = 'stickytax_get_id_from_name';
-		$_POST['term_name'] = uniqid();
-		$_POST['term_type'] = 'category';
+		$response = $this->make_ajax_request( [
+			'term_name' => uniqid(),
+		] );
 
-		try {
-			$this->_handleAjax( 'stickytax_get_id_from_name' );
-		} catch ( WPAjaxDieContinueException $e ) {
-			return;
-		}
-
-		// Check that the exception was thrown.
-		$response = json_decode( $this->_last_response );
-
-		$this->assertInternalType( 'object', $response );
-		$this->assertObjectHasAttribute( 'success', $response );
 		$this->assertFalse( $response->success );
-		$this->assertObjectHasAttribute( 'data', $response );
-		$this->assertObjectHasAttribute( 'errcode', $response->data );
 		$this->assertEquals( $response->data->errcode, 'NO_TERM_FOUND' );
 	}
 
+	/**
+	 * Send a request to the stickytax_get_id_from_name ajax handler.
+	 *
+	 * @param array $args Optional. Arguments to specify in the $_POST superglobal. Default is empty.
+	 * @return object The JSON-decoded ajax response.
+	 */
+	protected function make_ajax_request( $args = array() ) {
+		$_POST = array_merge( [
+			'nonce'     => wp_create_nonce( 'sticky-tax' ),
+			'action'    => 'stickytax_get_id_from_name',
+			'term_name' => 'Uncategorized',
+			'term_type' => 'category',
+		], $args );
+
+		/*
+		 * The WP Core testing suite will throw WPDieExceptions whenever Ajax responses are
+		 * returned, but we don't want these to prevent us from returning the last response.
+		 */
+		try {
+			$this->_handleAjax( 'stickytax_get_id_from_name' );
+
+		} catch ( WPDieException $e ) { // @codingStandardsIgnoreLine.
+			// Do nothing, proceed.
+		}
+
+		return json_decode( $this->_last_response );
+	}
 }
